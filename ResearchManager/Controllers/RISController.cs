54bf28e136b2bab798d1c2762a8245e1775611b6
MarcGi <marc.giannandrea@yahoo.co.uk>
2018-02-19 20:04:08 +0000

    Project Template

---
+++ ResearchManager/Controllers/RISController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ResearchManager.Controllers
+{
+    public class RISController : Controller
+    {
+        // GET: RIS
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ResearchManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ResearchManager.MvcApplication" Language="C#" %>
