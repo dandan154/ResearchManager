cf64e0a852617d9e63f043b7875915c25ae1d341
ConnorSword <connorsword25@googlemail.com>
2018-02-21 17:02:27 +0000

    Email template - working

---
+++ ResearchManager/Controllers/EmailController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using ResearchManager.Models;
+namespace ResearchManager.Controllers
+{
+    public class EmailController : Controller
+    {
+        // GET: Home  
+        public ActionResult SendEmail()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendEmail(EmailModel obj)
+        {
+
+            try
+            {
+                //Configuring webMail class to send emails  
+                //gmail smtp server  
+                WebMail.SmtpServer = "smtp.gmail.com";
+                //gmail port to send emails  
+                WebMail.SmtpPort = 587;
+                WebMail.SmtpUseDefaultCredentials = true;
+                //sending emails with secure protocol  
+                WebMail.EnableSsl = true;
+                //EmailId used to send emails from application  
+                WebMail.UserName = "connorsword25@googlemail.com";
+                WebMail.Password = "EmmaWoodhouse100";
+
+                //Sender email address.  
+                WebMail.From = "connorsword25@googlemail.com";
+
+                //Send email  
+                WebMail.Send(to: obj.ToEmail, subject: obj.EmailSubject, body: obj.EMailBody, cc: obj.EmailCC, bcc: obj.EmailBCC, isBodyHtml: true);
+                ViewBag.Status = "Email Sent Successfully.";
+            }
+            catch (Exception)
+            {
+                ViewBag.Status = "Problem while sending email, Please check details.";
+
+            }
+            return View();
+        }
+    }
+}
