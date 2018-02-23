0d4549cde08d6fdb906ec4d9ae217298b43cc661
scarnegie96 <deega123>
2018-02-23 18:40:17 +0000

    Change File Names for Creation and Upload

--- ResearchManager/Controllers/ResearchController.cs
+++ ResearchManager/Controllers/ResearchController.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace ResearchManager.Controllers
 {
@@ -34,6 +36,7 @@ namespace ResearchManager.Controllers
         [HttpPost]
         public ActionResult createProject(project model, HttpPostedFileBase file)
         {
+            int uID = Convert.ToInt32(Session["UserID"]);
             var allowedExtensions = new[] { ".xls", ".xlsx"};
             if (!allowedExtensions.Contains(Path.GetExtension(file.FileName)))
             {
@@ -46,7 +49,16 @@ namespace ResearchManager.Controllers
                 if (file.ContentLength > 0)
                 {
                     var fileName = Path.GetFileName(file.FileName);
-                    path = Path.Combine(Server.MapPath("~/App_Data/ExpenditureFiles"),fileName);
+                    var fileextension = Path.GetExtension(fileName);
+                    Random randInt = new Random();
+                    int rand1 = randInt.Next(1, 10000);
+                    int rand2 = randInt.Next(1, 10000);
+                    int rand3 = randInt.Next(1, 10000);
+                    String randOne = Convert.ToString(rand1);
+                    String randTwo = Convert.ToString(rand2);
+                    String randThree = Convert.ToString(rand3);
+                    String newName = randOne + randTwo + randThree + "." + fileextension;
+                    path = Path.Combine(Server.MapPath("~/App_Data/ExpenditureFiles"),newName);
                     file.SaveAs(path);
                 }
             }
@@ -61,13 +73,13 @@ namespace ResearchManager.Controllers
                 var db = new Entities();
                 db.projects.Add(new project
                 {
-                    userID = 1,
+                    userID = Convert.ToInt32(Session["UserID"]),
                     dateCreated = DateTime.Now.ToUniversalTime(),
                     projectStage = "Created",
                     pName = model.pName,
                     pAbstract = model.pAbstract,
                     pDesc = model.pDesc,
-                    projectFile = path
+                    projectFile = path,
                  });
                 db.SaveChanges();
                 ViewBag.Message = "Created Project";
